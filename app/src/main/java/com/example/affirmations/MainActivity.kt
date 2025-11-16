2b7f1fad3f2479d852f9db22bd5fe0f592a5ed2e
Berke Balci <berkebalcii38@gmail.com>
2025-11-16 22:47:18 +0100

    HomeWork5 has been pushed

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.affirmations
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RawRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,10 +28,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.MyApplicationTheme
 import com.example.myapplication.R
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Surface
+import androidx.compose.ui.platform.LocalLayoutDirection
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,22 +61,58 @@ class MainActivity : ComponentActivity() {
 fun AffirmationsApp(modifier: Modifier = Modifier) {
     var amountInput by remember { mutableStateOf("") }
     val amount = amountInput.toDoubleOrNull() ?: 0.0
-
-
-    Column(
-        modifier = modifier
+    val affirmationObject = Datasource()
+    val layoutDirection = LocalLayoutDirection.current
+    Surface(
+        modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 40.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = stringResource(R.string.calculate_tip),
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
+            .statusBarsPadding()
+            .padding(
+                start = WindowInsets.safeDrawing.asPaddingValues()
+                    .calculateStartPadding(layoutDirection),
+                end = WindowInsets.safeDrawing.asPaddingValues()
+                    .calculateEndPadding(layoutDirection),
+            )
+    )
+    {
+
+        AffirmationList(affirmationList = affirmationObject.loadAffirmations())
 
 
 
     }
 }
+
+@Composable
+fun AffirmationCard(
+    affirmation: Affirmation,
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Column {
+            Image(painter = painterResource(affirmation.imageResourceId),contentDescription = stringResource(affirmation.stringResourceId)
+                , modifier = Modifier.fillMaxWidth().height(194.dp), contentScale = ContentScale.Crop)
+            Text(text = stringResource(affirmation.stringResourceId), modifier = Modifier.padding(16.dp))
+
+
+        }
+    }
+}
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier){
+    LazyColumn(modifier = modifier) {
+        items(affirmationList){ affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+
+            )
+
+        }
+    }
+
+}
+
 
