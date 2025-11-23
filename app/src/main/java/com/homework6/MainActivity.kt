00b8f870332ebe66dc09ab9ce30cd95c832c0fad
Berke Balci <berkebalcii38@gmail.com>
2025-11-23 18:05:44 +0100

    HomeWork 6 has been created

---
+++ app/src/main/java/com/homework6/MainActivity.kt
@@ -0,0 +1,357 @@
+package com.homework6
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.core.content.ContextCompat
+import com.homework6.data.DataSource
+import com.homework6.model.Dessert
+import androidx.compose.ui.unit.dp
+import android.os.Bundle
+import android.provider.CalendarContract.Colors
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.homework6.ui.theme.HomeWork6Theme
+import androidx.compose.ui.graphics.Color
+private const val TAG = "MainActivity"
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG,"OnCreate mthod has been created")
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HomeWork6Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize().statusBarsPadding(),
+                    color = MaterialTheme.colorScheme.surface
+
+                    // statusBarsPadding sayesinde ekranin ustundeki status bar kismina uygulama yapmiyoruz.
+                ) {
+                    DessertClickerApp(desserts = DataSource.dessertList)
+                }
+
+                }
+            }
+        }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"OnStart method has been created")
+
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
+    }
+}
+
+
+@Composable
+private fun DessertClickerApp(
+    desserts: List<Dessert>
+) {
+    var revenue by remember { mutableStateOf(0) }
+    var dessertsSold by remember { mutableStateOf(0) }
+    val currentDessertIndex by remember { mutableStateOf(0) }
+    var currentDessertPrice by remember {
+        mutableStateOf(desserts[currentDessertIndex].price)
+    }
+    var currentDessertImageId by remember {
+        mutableStateOf(desserts[currentDessertIndex].imageId)
+    }
+    Scaffold(
+        topBar = {
+            val intentContext = LocalContext.current
+            val layoutDirection =
+                LocalLayoutDirection.current
+            DessertClickerAppBar(
+                onShareButtonClicked = {
+                    shareSoldDessertsInformation(
+                        intentContext = intentContext,
+                        dessertsSold = dessertsSold,
+                        revenue = revenue
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start =
+                            WindowInsets.safeDrawing.asPaddingValues().calculateStartPadding(layoutDirection),
+                        end =
+                            WindowInsets.safeDrawing.asPaddingValues()
+                                .calculateEndPadding(layoutDirection),
+                    )
+                    .background(MaterialTheme.colorScheme.primary)
+            )
+        }
+    ) { contentPadding ->
+        DessertClickerScreen(
+            revenue = revenue,
+            dessertsSold = dessertsSold,
+            dessertImageId = currentDessertImageId,
+            onDessertClicked = {
+// Update the revenue
+                revenue += currentDessertPrice
+                dessertsSold++
+// Show the next dessert
+                val dessertToShow = determineDessertsToShow(desserts, dessertsSold)
+                currentDessertImageId =
+                    dessertToShow.imageId
+                currentDessertPrice = dessertToShow.price
+            },
+            modifier = Modifier.padding(contentPadding)
+        )
+    }
+}
+@Composable
+fun DessertClickerScreen(
+    revenue: Int,
+    dessertsSold: Int,
+    @DrawableRes dessertImageId: Int,
+    onDessertClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.bakery_back),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        Column {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+            ) {
+                Image(
+                    painter = painterResource(dessertImageId),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(150.dp)
+                        .height(150.dp)
+                        .align(Alignment.Center)
+                        .clickable { onDessertClicked() },
+                    contentScale = ContentScale.Crop,
+                )
+            }
+            TransactionInfo(
+                revenue = revenue,
+                dessertsSold = dessertsSold,
+                modifier =
+                    Modifier.background(MaterialTheme.colorScheme.secondaryContainer
+                    )
+            )
+        }
+    }
+}
+@Composable
+private fun DessertClickerAppBar(
+    onShareButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            modifier = Modifier.padding(start = 150.dp),
+            color = MaterialTheme.colorScheme.onPrimary,
+            style = MaterialTheme.typography.titleLarge,
+        )
+        IconButton(
+            onClick = onShareButtonClicked,
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Share,
+                contentDescription =
+                    stringResource(R.string.share),
+                tint = Color.Black
+            )
+        }
+    }
+}
+
+fun determineDessertsToShow(
+    desserts: List<Dessert>,
+    dessertsSold: Int
+): Dessert{
+    var dessertToShow = desserts.first()
+    for(dessert in desserts){
+        if(dessertsSold>= dessert.startProductionAmount){
+            dessertToShow = dessert
+        }
+        else{
+            break
+        }
+    }
+    return dessertToShow
+    }
+
+private fun shareSoldDessertsInformation(
+    intentContext: Context,
+    dessertsSold: Int,
+    revenue: Int
+
+){
+    val sendIntent = Intent().apply {
+        action = Intent.ACTION_SEND
+        putExtra(
+            Intent.EXTRA_TEXT,
+            intentContext.getString(R.string.share_text, dessertsSold, revenue)
+        )
+        type = "text/plain"
+    }
+    val shareIntent = Intent.createChooser(sendIntent,null)
+    try{
+        ContextCompat.startActivity(intentContext, shareIntent, null)
+    }
+    catch (e: ActivityNotFoundException){
+        Toast.makeText( // Kullaniciya bilgi vermek icin
+            intentContext,
+            intentContext.getString(R.string.sharing_not_available),
+            Toast.LENGTH_LONG
+        ).show()
+
+    }
+
+
+}
+@Composable
+private fun RevenueInfo(revenue: Int, modifier: Modifier =
+    Modifier) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = stringResource(R.string.total_revenue),
+            style =
+                MaterialTheme.typography.headlineMedium,
+            color =
+                MaterialTheme.colorScheme.onSecondaryContainer
+        )
+        Text(
+            text = "$${revenue}"
+            ,
+            textAlign = TextAlign.Right,
+            style =
+                MaterialTheme.typography.headlineMedium,
+            color =
+                MaterialTheme.colorScheme.onSecondaryContainer
+        )
+    }
+}
+
+@Composable
+private fun DessertsSoldInfo(dessertsSold: Int, modifier:
+Modifier = Modifier) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = stringResource(R.string.dessert_sold),
+            style = MaterialTheme.typography.titleLarge,
+            color =
+                MaterialTheme.colorScheme.onSecondaryContainer
+        )
+        Text(
+            text = dessertsSold.toString(),
+            style = MaterialTheme.typography.titleLarge,
+            color =
+                MaterialTheme.colorScheme.onSecondaryContainer
+        )
+    }
+}
+
+@Composable
+private fun TransactionInfo(
+    revenue: Int,
+    dessertsSold: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        DessertsSoldInfo(
+            dessertsSold = dessertsSold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        RevenueInfo(
+            revenue = revenue,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+    }}
+
+
---
+++ app/src/main/java/com/homework6/model/Dessert.kt
@@ -0,0 +1,7 @@
+package com.homework6.model
+
+data class Dessert(
+    val imageId: Int,
+    val price: Int,
+    val startProductionAmount: Int
+)
